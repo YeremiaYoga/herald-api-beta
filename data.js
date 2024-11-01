ab5524aabc8c1b5f5a68be3fd65f54b68df7b31a
Yeremia Yoga <yogayeremia@gmail.com>
2024-11-01 15:24:51 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: "Cabin Forest in the River",
+    release: "2024/October",
+    thumbnail: "cabin-forest-in-the-river-thumbnail.jpg",
+    preview:
+      "https://heraldenterimentmedia.my.id/Cabin%20Forest%20in%20the%20River/preview.webp",
+    json: "scene-cabin-forest-in-the-river.json",
+    assets: [
+      {
+        id: 1,
+        name: "cabin-forest-in-the-river-top.webp",
+        url: "https://heraldenterimentmedia.my.id/Cabin%20Forest%20in%20the%20River/cabin-forest-in-the-river-top.webp",
+      },
+      {
+        id: 2,
+        name: "cabin-forest-in-the-river-bottom.webp",
+        url: "https://heraldenterimentmedia.my.id/Cabin%20Forest%20in%20the%20River/cabin-forest-in-the-river-bottom.webp",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Ruined Castle Within Forest",
+    release: "2024/October",
+    thumbnail: "ruined-castle-within-forest-thumbnail.jpg",
+    preview:
+      "https://heraldenterimentmedia.my.id/Ruined%20Castle%20Within%20Forest/preview.webp",
+    json: "scene-ruined-castle-within-forest.json",
+    assets: [
+      {
+        id: 1,
+        name: "ruined-castle-within-forest-left.webp",
+        url: "https://heraldenterimentmedia.my.id/Ruined%20Castle%20Within%20Forest/ruined-castle-within-forest-left.webp",
+      },
+      {
+        id: 2,
+        name: "ruined-castle-within-forest-right.webp",
+        url: "https://heraldenterimentmedia.my.id/Ruined%20Castle%20Within%20Forest/ruined-castle-within-forest-right.webp",
+      },
+    ],
+  },
+];
+
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors"); // Import CORS
+const app = express();
+const port = 3000;
+const data = require("./data");
+const axios = require("axios");
+const fs = require("fs");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use("/data", express.static(path.join(__dirname, "data")));
+
+app.get("/api/data", (req, res) => {
+  res.json(data);
+});
+
+app.get("/download", async (req, res) => {
+  const { url } = req.query;
+
+  console.log(url);
+
+  if (!url) {
+    return res.status(400).send("No URL provided");
+  }
+
+  try {
+    const response = await axios({
+      method: "get",
+      url: url,
+      responseType: "stream",
+    });
+
+    const totalLength = response.headers["content-length"];
+    let downloadedLength = 0;
+
+    res.setHeader("Content-Type", response.headers["content-type"]);
+
+    response.data.on("data", (chunk) => {
+      downloadedLength += chunk.length;
+      const progress = ((downloadedLength / totalLength) * 100).toFixed(2);
+      console.log(`Download progress: ${progress}%`);
+    });
+
+    response.data.pipe(res);
+
+    response.data.on("end", () => {
+      console.log("Download complete");
+    });
+
+    response.data.on("error", (error) => {
+      console.error("Error during download:", error);
+      res.status(500).send("Error during download");
+    });
+  } catch (error) {
+    console.error("Download failed:", error.message);
+    res.status(500).send("Download failed: " + error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
